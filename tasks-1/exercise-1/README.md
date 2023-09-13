a4a27d36fa19c46fc40e2a220752aa5481beb38b
Aspandiyar933 <ggg.megadoom.lol@gmail.com>
2023-09-13 12:51:26 +0600

    test 2

---
+++ tasks-1/exercise-1/README.md
@@ -0,0 +1,11 @@
+# exercise-1
+
+# Give a input two digits and output sum 
+
+# Input 
+
+# 1, 1
+
+# Output
+
+# 2
---
+++ tasks-1/exercise-1/test.py
@@ -0,0 +1,19 @@
+import unittest
+from code import simple_sum
+
+class TestSimpleSum(unittest.TestCase):
+
+    def test_simple_sum_positive_numbers(self):
+        result = simple_sum(2, 3)
+        self.assertEqual(result, 5)
+
+    def test_simple_sum_negative_numbers(self):
+        result = simple_sum(-2, -3)
+        self.assertEqual(result, -5)
+
+    def test_simple_sum_mixed_numbers(self):
+        result = simple_sum(2, -3)
+        self.assertEqual(result, -1)
+
+if __name__ == '__main__':
+    unittest.main()
